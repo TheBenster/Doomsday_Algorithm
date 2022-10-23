b3da22eeda1b26bb26a4da03e498b5ba92f5b6b6
Ben Beaver <bbeaver93@gmail.com>
2022-10-22 23:43:36 -0400

    cleaning up with random day and calculation method

---
+++ Classing.py
@@ -0,0 +1,10 @@
+from doomsday import day, days, month, year, random_day
+
+
+class Calendar(random_day):
+    def print_random_year(random_day):
+        return random_day
+
+test = Calendar()
+
+test.print_random_year()
--- doomsday.py
+++ doomsday.py
@@ -1,35 +1,42 @@
 from random import randrange, choice
-day = randrange(1,30)
-days = { # Numbers signify abstraction of days
+class Calendar():
+    def random_day(day,  month, year, random_day):
+        day = randrange(1,30)
+
+        month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        year = randrange(1900, 1999)
+        random_day = print(f'Your date is {choice(month)} {day}, {year}')
+        return random_day
+    def doomsday_calculation(anchorDay, year):
+        yearString = str(year)
+        yearLength = len(yearString)
+        lastTwo = int(yearString[yearLength - 2: yearLength])
+        result = lastTwo / 12
+        remain = lastTwo % 12
+        leapYear = remain / 4
+        doomsday = result + remain + leapYear
+        print(year)
+        print(int(doomsday))
+
+test = Calendar()
+
+test.doomsday_calculation(year=randrange(1900,1999))
+
+daysDict = { # Numbers signify abstraction of days
     'Sunday' : 0, 'Monday' : 1, 'Tuesday' : 2, "Wednesday": 3, "Thursday" : 4, "Friday": 5, "Saturday" : 6, 'Sunday' : 7 
 }
-month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-# print(choice(month))
-year = randrange(1900, 1999)
-
-print(f'Your date is {choice(month)} {day}, {year}')
 
 anchorNum = { # Reference doomsday
     2000 : 2, 1900 : 3,
     1800 : 5, 1700 : 0
 }
 
-yearString = str(year)
-yearLength = len(yearString)
-lastTwo = int(yearString[yearLength - 2: yearLength]) # last two digits of year
+ # last two digits of year
 
-def doomsday_calculation(anchorDay):
-    for _ in year:
-        if year == anchorNum.keys(2000):
-            anchorDay = days['Tuesday']
-    result = lastTwo / 12
-    remain = lastTwo % 12
-    leapYear = remain / 4
-    doomsday = result + remain + leapYear
-    print(int(doomsday))
+
     
-if year < 2000: # If year is  1900- the anchor day is tuesday (The doomsday for the year 1900 is a tuesday)
-    anchorDay = days['Tuesday'] # anchorDay represents the doomsday for that particular year. Ex. 1900 is a tuesday, 1901 is a wednesday
+'''if year < 2000: # If year is  1900- the anchor day is tuesday (The doomsday for the year 1900 is a tuesday)
+    anchorDay = daysDict['Tuesday'] # anchorDay represents the doomsday for that particular year. Ex. 1900 is a tuesday, 1901 is a wednesday
     result =  lastTwo / 12 # Finds how many times 12 goes into lastTwo (last two digits of year)
     remain = lastTwo % 12 # Finds the remainder ex(1962, 60 goes into 12 5 times with a 2 remainder)
     leapYear = remain / 4 # Finds how many times the leap year (4) goes into the remainder
@@ -48,22 +55,22 @@ if year < 2000: # If year is  1900- the anchor day is tuesday (The doomsday for 
 
     mainResult = int(anchorDay + answer)
     print(mainResult)
-    if mainResult == days['Sunday']:
+    if mainResult == daysDict['Sunday']:
         print(f'The doomsday for {year} is Sunday')
-    elif mainResult == days['Monday']:
+    elif mainResult == daysDict['Monday']:
         print(f"The dooms day for {year} is Monday")
-    elif mainResult == days['Tuesday']:
+    elif mainResult == daysDict['Tuesday']:
         print(f"The doomsday for {year} is Tuesday") 
-    elif mainResult == days['Wednesday']:
+    elif mainResult == daysDict['Wednesday']:
         print(f"The doomsday for {year} is Wednesday")
-    elif mainResult == days['Thursday']:
+    elif mainResult == daysDict['Thursday']:
         print(f"the doomsday for {year} is Thursday")
-    elif mainResult == days['Friday']:
+    elif mainResult == daysDict['Friday']:
         print(f"The doomsday for {year} is Friday")
         
     # print(int(answer))
 
-
+'''
 
 # print(anchorNum[2000])
 
